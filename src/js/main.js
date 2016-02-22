9f9f8ba3c90009940f79ea9e820455e886fe319b
janlonden <jjlonden@gmail.com>
2016-02-22 18:43:21 +0100

    inline script

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 'use strict'
 
-let mykuvaify = kuvaify({
-  linkElement: '#links'
-})
 let demoButton = document.querySelector('#demo-button')
 let demoLink = document.querySelector('#demo-link')
 
